9cf56545446259b1fd74838f5ee084b5f3f2ab01
Joice Araújo <joicearaujoedu@gmail.com>
2022-04-11 13:09:14 -0300

    Adicionado componente Title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
+import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -8,6 +10,8 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Title />
+        <PlanetCard />
       </div>);
   }
 }
