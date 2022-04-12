3c93dcf2b1ce241925f592e3288f1baf3fb97c17
Joice Araújo <joicearaujoedu@gmail.com>
2022-04-11 22:44:08 -0300

    concertando mais erros no lint

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    const headline = this.props;
+    const { headline } = this.props;
     return (
       <h2>
         { headline }
@@ -13,6 +13,6 @@ class Title extends React.Component {
 }
 export default Title;
 
-Title.prototype = {
+Title.propTypes = {
   headline: PropTypes.string.isRequired,
 };
